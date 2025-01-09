9504279032e2ea4bc6e87987b12d20838a98dcb0
James Joseph <98562132+JW796@users.noreply.github.com>
2025-01-09 10:57:19 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 className="text-3xl text-center py-6 my-2">useContext</h1>
+  );
+}
+
+export default App;
---
+++ src/components/Podcasts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Podcasts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Podcasts
+
