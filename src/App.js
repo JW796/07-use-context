0261fd949ea9febd22b669875814fca07fd12224
James Joseph <98562132+JW796@users.noreply.github.com>
2025-01-10 11:19:03 -0500

    React router setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Podcasts from "./components/Podcasts";
+import Recommendations from "./components/Recommendations";
+import { UserContext } from "./Contexts/UserContext";
+
 function App() {
   return (
-    <h1 className="text-3xl text-center py-6 my-2">useContext</h1>
+    <>
+      <h1 className="text-3xl text-center py-6 my-2">useContext</h1>
+
+      <Router>
+        <UserContext.Provider value="Star Lord">
+          <Routes>
+            <Route path="/dashboard" element={Dashboard} />
+            <Route path="/podcasts" element={Podcasts} />
+            <Route path="/recomendations" element={Recommendations} />
+          </Routes>
+        </UserContext.Provider>
+      </Router>
+    </>
   );
 }
 
