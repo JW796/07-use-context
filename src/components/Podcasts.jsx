2edd4051089ffb8a69d69b7cac3959a7cefab476
James Joseph <98562132+JW796@users.noreply.github.com>
2025-01-13 00:58:45 -0500

    Render context to components

--- src/components/Podcasts.jsx
+++ src/components/Podcasts.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../Contexts/UserContext'
 
 const Podcasts = () => {
+  const user = useContext(UserContext);
   return (
     <div>
-      
+      <h1>Hello {user}</h1>
+      <h2>Your Podcasts</h2>
     </div>
   )
 }
--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { UserContext } from "../Contexts/UserContext";
 
 const Recommendations = () => {
+  const user = useContext(UserContext);
   return (
     <div>
-      
+      <h1>Hello {user}</h1>
+      <h2>Your Recommendations</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Recommendations
+export default Recommendations;
